6a708c64e39ceb8081b76464aa1ce653b66804f2
Manmohan Singh <er.manmohansingh7@gmail.com>
2023-03-06 01:18:53 -0500

    Initial Commit

---
+++ VoyaceAdmin/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VoyaceAdmin
+//
+//  Created by Manmohan Singh on 2023-03-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VoyaceAdmin/VoyaceAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoyaceAdminApp.swift
+//  VoyaceAdmin
+//
+//  Created by Manmohan Singh on 2023-03-06.
+//
+
+import SwiftUI
+
+@main
+struct VoyaceAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
